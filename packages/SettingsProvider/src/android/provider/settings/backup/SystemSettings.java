eb79c83d3a64a2fc6e5dd0e978992f2990fc7465
John Macka <macka2004@gmail.com>
2025-08-03 12:20:32 +1000

    Revert "SystemUI: Allow customizing footer text [1/2]"
    This reverts commit d5ee2fc6341238e8795d5df85878e3057243110c.

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -128,7 +128,6 @@ public class SystemSettings {
                 Settings.System.CV_ENABLED,
                 Settings.System.OMNI_ADVANCED_REBOOT,
                 Settings.System.QS_FOOTER_TEXT_SHOW,
-                Settings.System.QS_FOOTER_TEXT_STRING,
                 Settings.System.LOCKSCREEN_BATTERY_INFO,
                 Settings.System.NETWORK_TRAFFIC_STATE,
                 Settings.System.NETWORK_TRAFFIC_TYPE,
--- packages/SettingsProvider/src/android/provider/settings/validators/SystemSettingsValidators.java
+++ packages/SettingsProvider/src/android/provider/settings/validators/SystemSettingsValidators.java
@@ -279,7 +279,6 @@ public class SystemSettingsValidators {
                 new InclusiveIntegerRangeValidator(0, 1));
         VALIDATORS.put(System.OMNI_ADVANCED_REBOOT, BOOLEAN_VALIDATOR);
         VALIDATORS.put(System.QS_FOOTER_TEXT_SHOW, BOOLEAN_VALIDATOR);
-        VALIDATORS.put(System.QS_FOOTER_TEXT_STRING, ANY_STRING_VALIDATOR);
         VALIDATORS.put(System.LOCKSCREEN_BATTERY_INFO, BOOLEAN_VALIDATOR);
         VALIDATORS.put(System.NETWORK_TRAFFIC_STATE, BOOLEAN_VALIDATOR);
         VALIDATORS.put(System.NETWORK_TRAFFIC_TYPE, new InclusiveIntegerRangeValidator(0, 4));
