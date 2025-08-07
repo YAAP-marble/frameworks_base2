7f959e8a272e373a48ce05e9be880f95147f79d5
Abhay Singh Gill <abhaygill017@gmail.com>
2025-08-07 18:00:24 +0530

    SystemUI: Fixup fonts not updating on new compose QS
    Switch to TextView for now
    
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>

--- packages/SystemUI/compose/features/src/com/android/systemui/qs/footer/ui/compose/FooterActions.kt
+++ packages/SystemUI/compose/features/src/com/android/systemui/qs/footer/ui/compose/FooterActions.kt
@@ -92,6 +92,7 @@ import com.android.systemui.qs.ui.composable.QuickSettings
 import com.android.systemui.qs.ui.composable.QuickSettingsTheme
 import com.android.systemui.qs.ui.compose.borderOnFocus
 import com.android.systemui.res.R
+import com.android.systemui.text.CompatText
 import kotlinx.coroutines.launch
 
 @Composable
@@ -361,7 +362,7 @@ private fun NumberButton(
                     .clip(CircleShape)
                     .indication(interactionSource, LocalIndication.current)
             ) {
-                Text(
+                CompatText(
                     number.toString(),
                     modifier =
                         Modifier.align(Alignment.Center).semantics {
@@ -426,7 +427,7 @@ private fun TextButton(
                 colorAttr(R.attr.onShadeInactiveVariant),
             )
 
-            Text(
+            CompatText(
                 text,
                 Modifier.weight(1f),
                 style =
